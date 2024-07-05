38f6f1ee9da9023b8c7722bbd8e4a8950151a270
shahaf-Segal <shahaf100@outlook.co.il>
2024-07-05 13:48:53 +0300

    [PA-9] Added FormBlock for ease of use editing phone

---
+++ src/components/atoms/FormBlock/FormBlock.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./FormBlock.module.css";
+
+interface FormBlock {
+  inputName: string;
+  type: string;
+  labelText: string;
+  changeFunc?: (event: React.ChangeEvent) => void;
+  rows: number;
+  placeholder: string;
+  required: boolean;
+  defaultValue: string | number | undefined;
+  rtl?: boolean;
+  value: string;
+}
+
+const FormBlock: React.FC<FormBlock> = ({
+  inputName,
+  type = "text",
+  labelText,
+  changeFunc = () => {},
+  placeholder = "",
+  defaultValue,
+  rows = 1,
+  required = false,
+  rtl = true,
+  value,
+}) => {
+  return (
+    <>
+      <label htmlFor={inputName} className={styles["form-label"]}>
+        <strong>{labelText}</strong>
+      </label>
+      {type === "textarea" ? (
+        <textarea
+          dir={rtl ? "rtl" : "ltr"}
+          id={inputName}
+          name={inputName}
+          onChange={changeFunc}
+          className={styles["form-input"]}
+          {...(defaultValue && { defaultValue })}
+          {...(value && { value })}
+          rows={rows}
+          placeholder={placeholder}
+          required={required}
+        />
+      ) : (
+        <input
+          dir={rtl ? "rtl" : "ltr"}
+          type={type}
+          id={inputName}
+          name={inputName}
+          onChange={changeFunc}
+          className={styles["form-input"]}
+          defaultValue={defaultValue}
+          placeholder={placeholder}
+          required={required}
+          {...(defaultValue && { defaultValue })}
+          {...(value && { value })}
+        />
+      )}
+    </>
+  );
+};
+
+export default FormBlock;
