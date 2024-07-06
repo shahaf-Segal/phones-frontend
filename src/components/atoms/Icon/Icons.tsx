6d1cfa7671d91818b12556aa55d40e5dd96e9e27
shahaf-Segal <shahaf100@outlook.co.il>
2024-07-06 18:25:59 +0300

    [PA-8] removed icons and added new

--- src/components/atoms/Icon/Icons.tsx
+++ src/components/atoms/Icon/Icons.tsx
@@ -1,91 +1,88 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import { lazy } from 'react';
-
-const AddJobIcon = lazy(async () => import('./icons/AddJobIcon.svg?react'));
-
-
+import { lazy } from "react";
+const AddIcon = lazy(async () => import("./icons/AddIcon.svg?react"));
 const ArrowDownIcon = lazy(
-  async () => import('./icons/ArrowDownIcon.svg?react'),
+  async () => import("./icons/ArrowDownIcon.svg?react")
 );
-const BellIcon = lazy(async () => import('./icons/BellIcon.svg?react'));
+const BellIcon = lazy(async () => import("./icons/BellIcon.svg?react"));
 
-const BulbIcon = lazy(async () => import('./icons/BulbIcon.svg?react'));
+const BulbIcon = lazy(async () => import("./icons/BulbIcon.svg?react"));
 
-const TrophyIcon = lazy(async () => import('./icons/TrophyIcon.svg?react'));
- 
+const TrophyIcon = lazy(async () => import("./icons/TrophyIcon.svg?react"));
+
 const ChampagneGlassesIcon = lazy(
-  async () => import('./icons/ChampagneGlassesIcon.svg?react'),
+  async () => import("./icons/ChampagneGlassesIcon.svg?react")
 );
 const CircleRegularIcon = lazy(
-  async () => import('./icons/CircleRegularIcon.svg?react'),
+  async () => import("./icons/CircleRegularIcon.svg?react")
 );
-const CommentIcon = lazy(async () => import('./icons/CommentIcon.svg?react'));
+const CommentIcon = lazy(async () => import("./icons/CommentIcon.svg?react"));
 
 const DownloadArrowIcon = lazy(
-  async () => import('./icons/DownloadArrowIcon.svg?react'),
+  async () => import("./icons/DownloadArrowIcon.svg?react")
 );
-const EditBtnIcon = lazy(async () => import('./icons/EditBtnIcon.svg?react'));
+const EditBtnIcon = lazy(async () => import("./icons/EditBtnIcon.svg?react"));
 
-const EnvelopeIcon = lazy(async () => import('./icons/EnvelopeIcon.svg?react'));
+const EnvelopeIcon = lazy(async () => import("./icons/EnvelopeIcon.svg?react"));
 
 const EyeIconWithLine = lazy(
-  async () => import('./icons/EyeIconWithLine.svg?react'),
+  async () => import("./icons/EyeIconWithLine.svg?react")
 );
-const FilterIcon = lazy(async () => import('./icons/FilterIcon.svg?react'));
+const FilterIcon = lazy(async () => import("./icons/FilterIcon.svg?react"));
 
-const GemIcon = lazy(async () => import('./icons/GemIcon.svg?react'));
+const GemIcon = lazy(async () => import("./icons/GemIcon.svg?react"));
 
-const GitHubIcon = lazy(async () => import('./icons/GitHubIcon.svg?react'));
+const GitHubIcon = lazy(async () => import("./icons/GitHubIcon.svg?react"));
 
-const GlobeIcon = lazy(async () => import('./icons/GlobeIcon.svg?react'));
+const GlobeIcon = lazy(async () => import("./icons/GlobeIcon.svg?react"));
 
-const EmptyHeartIcon = lazy(async () => import('./icons/HeartIcon.svg?react'));
+const EmptyHeartIcon = lazy(async () => import("./icons/HeartIcon.svg?react"));
 
 const FullHeartIcon = lazy(
-  async () => import('./icons/FullHeartIcon.svg?react'),
+  async () => import("./icons/FullHeartIcon.svg?react")
 );
 
 const HollowClockIcon = lazy(
-  async () => import('./icons/HollowClockIcon.svg?react'),
+  async () => import("./icons/HollowClockIcon.svg?react")
 );
-const LinkedInIcon = lazy(async () => import('./icons/LinkedInIcon.svg?react'));
+const LinkedInIcon = lazy(async () => import("./icons/LinkedInIcon.svg?react"));
 
 const MagnifyingGlassIcon = lazy(
-  async () => import('./icons/MagnifyingGlassIcon.svg?react'),
+  async () => import("./icons/MagnifyingGlassIcon.svg?react")
 );
-const MessageIcon = lazy(async () => import('./icons/MessageIcon.svg?react'));
+const MessageIcon = lazy(async () => import("./icons/MessageIcon.svg?react"));
 
-const OpenEyeIcon = lazy(async () => import('./icons/OpenEyeIcon.svg?react'));
+const OpenEyeIcon = lazy(async () => import("./icons/OpenEyeIcon.svg?react"));
 
 const PaginationDotIcon = lazy(
-  async () => import('./icons/PaginationDotIcon.svg?react'),
+  async () => import("./icons/PaginationDotIcon.svg?react")
 );
 const PhoneCallIcon = lazy(
-  async () => import('./icons/PhoneCallIcon.svg?react'),
+  async () => import("./icons/PhoneCallIcon.svg?react")
 );
-const PositionIcon = lazy(async () => import('./icons/PositionIcon.svg?react'));
+const PositionIcon = lazy(async () => import("./icons/PositionIcon.svg?react"));
 
-const SparksIcon = lazy(async () => import('./icons/SparksIcon.svg?react'));
+const SparksIcon = lazy(async () => import("./icons/SparksIcon.svg?react"));
 
-const StarIcon = lazy(async () => import('./icons/StarIcon.svg?react'));
+const StarIcon = lazy(async () => import("./icons/StarIcon.svg?react"));
 
-const UserIcon = lazy(async () => import('./icons/UserIcon.svg?react'));
+const UserIcon = lazy(async () => import("./icons/UserIcon.svg?react"));
 
-const BurgerIcon = lazy(async () => import('./icons/BurgerIcon.svg?react'));
+const BurgerIcon = lazy(async () => import("./icons/BurgerIcon.svg?react"));
 
-const DumpsterIcon = lazy(async () => import('./icons/DumpsterIcon.svg?react'));
+const DumpsterIcon = lazy(async () => import("./icons/DumpsterIcon.svg?react"));
 
-const GoBackArrowIcon = lazy(async () => import('./icons/GoBackArrowIcon.svg?react'));
+const GoBackArrowIcon = lazy(
+  async () => import("./icons/GoBackArrowIcon.svg?react")
+);
 
-const AddFileIcon = lazy(async () => import('./icons/AddFileIcon.svg?react'));
+const PlusIcon = lazy(async () => import("./icons/PlusIcon.svg?react"));
 
-const PlusIcon = lazy(async () => import('./icons/PlusIcon.svg?react'));
-
-const MinusIcon = lazy(async () => import('./icons/MinusIcon.svg?react'));
+const MinusIcon = lazy(async () => import("./icons/MinusIcon.svg?react"));
 
 export const icons = {
-  AddJobIcon,
+  AddIcon,
   ArrowDownIcon,
   BellIcon,
   BulbIcon,
@@ -117,7 +114,6 @@ export const icons = {
   GoBackArrowIcon,
   TrophyIcon,
   DumpsterIcon,
-  AddFileIcon,
   PlusIcon,
-  MinusIcon
+  MinusIcon,
 };
