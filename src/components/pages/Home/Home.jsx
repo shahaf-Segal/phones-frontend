bf017d49ddb550d9e396362d8fd69438070fe452
shahaf-Segal <shahaf100@outlook.co.il>
2024-07-07 13:02:20 +0300

    [PA-7] start working on paginatiom

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,33 +1,42 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { baseUrl } from "../../../utils/utils";
-import Popover from "../../atoms/Popover/Popover";
-// import Pagination from "../../molecules/Pagination/Pagination";
 import useQuery from "../../../hooks/useQuery";
+import { baseUrl } from "../../../utils/utils";
 import Button from "../../atoms/Button/Button";
 import Icon from "../../atoms/Icon/Icon";
+import Popover from "../../atoms/Popover/Popover";
+import Pagination from "../../molecules/Pagination/Pagination";
 import Table from "../../molecules/Table/Table";
 import PhoneEditor from "../../PhoneEditor/PhoneEditor";
+import styles from "./Home.module.css";
 
 function Home() {
   const [phonesArray, setPhonesArray] = useState([]);
+  const [pagesData, setPagesData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [popoverElement, setPopoverElement] = useState();
+
   const query = useQuery();
 
   const getPhones = async () => {
     const toastID = toast.loading("Loading Phones");
     setIsLoading(true);
     try {
-      const res = await axios.get(`${baseUrl}phones?/`);
-      setPhonesArray(res.data);
+      const { data } = await axios.get(`${baseUrl}phones?/`);
+      setPhonesArray(data.phones);
+      setPagesData(data.pages);
       toast.dismiss(toastID);
     } catch (error) {
       toast.error(error.message, { id: toastID });
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const closePopover = () => {
+    setPopoverElement(null);
+    getPhones();
   };
 
   //add query later
@@ -83,7 +92,9 @@ function Home() {
       propsMapping: (data) => {
         return {
           onClick: () => {
-            setPopoverElement(<PhoneEditor phoneID={data._id} />);
+            setPopoverElement(
+              <PhoneEditor phoneID={data._id} closePopover={closePopover} />
+            );
           },
         };
       },
@@ -92,16 +103,24 @@ function Home() {
 
   return (
     <div>
-      {isLoading || <Table columns={columns} data={phonesArray} />}
-      {/* <Pagination /> */}
+      <div className={styles["top-bar"]}>
+        <div className={styles["top-bar-btn"]}>
+          <Button
+            text="Add Phone"
+            icon={<Icon name="AddIcon" color="var(--light-text-color)" />}
+            onClick={() => {
+              setPopoverElement(<PhoneEditor closePopover={closePopover} />);
+            }}
+            fullWidth
+          />
+        </div>
+      </div>
+      <div className={styles["table-container"]}>
+        {isLoading || <Table columns={columns} data={phonesArray} />}
+        <Pagination currentPage={1} totalPages={3} />
+      </div>
 
-      <Popover
-        isPopoverOpen={Boolean(popoverElement)}
-        onClose={() => {
-          setPopoverElement(null);
-          getPhones();
-        }}
-      >
+      <Popover isPopoverOpen={Boolean(popoverElement)} onClose={closePopover}>
         {popoverElement}
       </Popover>
     </div>
