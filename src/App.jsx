171cf28adef74134234a3f31807c7446621bf8e7
shahaf-Segal <shahaf100@outlook.co.il>
2024-07-08 18:57:22 +0300

    remove auth from routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<div></div>} />
           </Routes>
         </main>
         <AppToaster />
